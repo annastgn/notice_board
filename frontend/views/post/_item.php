f6e2304e9582ac6971a40a48c77293473c90e131
annastgn <annastgn@gmail.com>
2019-12-18 02:14:45 +0700

    change migrations, create PostShow

--- frontend/views/post/_item.php
+++ frontend/views/post/_item.php
@@ -3,7 +3,7 @@ use yii\helpers\Html;
 use yii\helpers\HtmlPurifier;
 ?>
     <div class="post">
-    <a><h1><?= Html::encode($model->title) ?></h1></a>
+    <a><h1><?= Html::encode($model->title) ?></h1>git</a>
     <p><?= Html::encode($model->city) ?></p>
 
     <?= HtmlPurifier::process($model->content) ?>
